6bde57a633a42fbfbf16e165dd267fa5f7048465
herberthsfc <76819767+herberthsfc@users.noreply.github.com>
2021-01-20 12:48:30 -0300

    Add files via upload

---
+++ src/darkmenu.js
@@ -0,0 +1,27 @@
+const darkmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do HDBOT.exe:*
+
+➸ *${prefix}comandos*
+➸ *${prefix}dono*
+
+╔════════════════════
+  BY: *Herberth*
+  DUVIDAS? 👇
+  INSTA: @herberthsfc
+╚════════════════════`
+}
+
+exports.darkmenu = darkmenu
+
+
+
+
+
+
+
+
